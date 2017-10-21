ffc17da66d496bc5de53c5727a942e3f9df7c7f5
dingzhong <dingzhong55555@gmail.com>
2017-10-22 00:20:40 +0800

    change product quantty

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -6,4 +6,18 @@ class CartItemsController < ApplicationController
     redirect_to :back
     flash[:warning] = "已删除商品#{@product.title}"
   end
+
+  def update
+    @cart_item = current_cart.cart_items.find_by(product_id: params[:id])
+    @product = @cart_item.product
+    @cart_item.update(cart_item_params)
+    redirect_to :back
+    flash[:warning] = "已更新商品#{@product.title}的数量"
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
+  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,9 +10,13 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
-    current_cart.add_product_to_cart(@product)
+    if !current_cart.products.include?(@product)
+      current_cart.add_product_to_cart(@product)
+      flash[:warning] = "已将#{@product.title}加入购物车"
+    else
+      flash[:warning] = "#{@product.title}已存在于购物车中"
+    end
     redirect_to :back
-    flash[:alert] = "已加入购物车"
   end
 
 end
