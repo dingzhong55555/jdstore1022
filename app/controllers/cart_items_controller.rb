f51b2c8369a756cd259677212b5079077f434f1c
dingzhong <dingzhong55555@gmail.com>
2017-10-22 00:32:57 +0800

    cannot change quantity which exceed stock

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -10,9 +10,14 @@ class CartItemsController < ApplicationController
   def update
     @cart_item = current_cart.cart_items.find_by(product_id: params[:id])
     @product = @cart_item.product
-    @cart_item.update(cart_item_params)
+    if @cart_item.quantity <= @product.quantity
+      @cart_item.update(cart_item_params)
+      flash[:notice] = "已更新商品#{@product.title}的数量"
+    else
+      flash[:warning] = "商品#{@product.title}的数量不足"
+    end
+
     redirect_to :back
-    flash[:warning] = "已更新商品#{@product.title}的数量"
   end
 
   private
